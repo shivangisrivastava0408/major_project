611b44ed1e746b748d3f055ffaa4067dd5f77c06
Shivangi Srivastava <shivangidhn1998@gmail.com>
2021-05-01 23:26:53 +0530

    Update settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '+sj(1p+dgej8xlie_l4mc=b$6!$s4de+r_bl30^0x!**0n(^8f'
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
